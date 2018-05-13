6bab26139ea6e8614ac0c4040438083e8ca4fdb5
Serge Nahas <sergenhs@gmail.com>
2018-05-12 23:16:47 -0400

    Added initial install.sh script to machine setup

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# =======================================================
+# This script is not ready to use yet, until changes are
+# made to check for the disto first in order to use
+# the correct package manager commands
+# =======================================================
+sudo apt install vim
+sudo apt install tmux
+sudo apt install git
+
+# Configure git
+git config --global user.name "Serge Nahas"
+git config --global user.email "sergenhs@gmail.com"
